4ea009a62d97a70a92278bfc38166d73abfa284a
anatol <anatol@beam-mw.com>
2021-06-03 15:21:39 +0300

    wallet-api test

--- complete/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
+++ complete/src/main/java/com/example/consumingrest/ConsumingRestApplication.java
@@ -8,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
@@ -26,9 +31,16 @@ public class ConsumingRestApplication {
 	@Bean
 	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
 		return args -> {
-			Quote quote = restTemplate.getForObject(
-					"https://quoters.apps.pcfone.io/api/random", Quote.class);
-			log.info(quote.toString());
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			//String request = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"wallet_status\"}";
+			String request = "{\"jsonrpc\":\"2.0\",\"id\":123,\"method\":\"tx_send\",\"params\":{\"value\":12342342,\"address\":\"472e17b0419055ffee3b3813b98ae671579b0ac0dcd6f1a23b11a75ab148cc67\"}}";
+			HttpEntity<String> entity = new HttpEntity<String>(request, headers);
+
+			
+			ResponseEntity<String> response = restTemplate.exchange("http://127.0.0.1:9100/api/wallet", HttpMethod.POST, entity, String.class);
+			
+			log.info(response.getBody());
 		};
 	}
 }
